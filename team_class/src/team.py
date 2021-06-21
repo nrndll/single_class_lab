95e21e50204170b50361cb1fb5ef987cfda3a3a2
Nathan <nathan.rendall@gmail.com>
2021-06-21 16:16:02 +0100

    points property and play_game() method added to Team class

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -3,6 +3,7 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
+        self.points = 0
 
     def add_player(self, new_player):
         self.players.append(new_player)
@@ -13,3 +14,7 @@ class Team:
             if player == name:
                 return True
         return team_has_player
+
+    def play_game(self, win):
+        if win:
+            self.points += 3
